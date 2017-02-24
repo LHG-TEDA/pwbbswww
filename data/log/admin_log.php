3e5f83a81b400934d81704cc3f5bb20033a61e67
lhg <2323221325@qq.com>
2017-02-25 00:56:09 +0800

    lhg

---
+++ data/log/admin_log.php
@@ -0,0 +1,46 @@
+<?php die;?>
+|admin|Login Successful|127.0.0.1|1487780366|/pwbbswww/admin.php?a=login|username=admin, password=***, submit=, csrf_token=5e8fac4f660b56ed|
+|admin|verify/verify/run|127.0.0.1|1487780382|/pwbbswww/admin.php?m=verify&c=verify||
+|admin|verify/verify/set|127.0.0.1|1487780416|/pwbbswww/admin.php?m=verify&c=verify&a=set||
+|admin|config/pay/run|127.0.0.1|1487780435|/pwbbswww/admin.php?m=config&c=pay||
+|admin|credit/credit/run|127.0.0.1|1487780446|/pwbbswww/admin.php?m=credit&c=credit||
+|admin|Login Successful|127.0.0.1|1487857079|/pwbbswww/admin.php?a=login|username=admin, password=***, submit=, csrf_token=e630b185c676b5f3|
+|admin|tag/manage/run|127.0.0.1|1487857126|/pwbbswww/admin.php?m=tag&c=manage||
+|admin|bbs/article/run|127.0.0.1|1487857210|/pwbbswww/admin.php?m=bbs&c=article||
+|admin|design/push/run|127.0.0.1|1487857731|/pwbbswww/admin.php?m=design&c=push||
+|admin|design/push/status|127.0.0.1|1487857735|/pwbbswww/admin.php?status=1&m=design&c=push&a=status||
+|admin|design/push/status|127.0.0.1|1487857737|/pwbbswww/admin.php?status=2&m=design&c=push&a=status||
+|admin|design/push/shield|127.0.0.1|1487857740|/pwbbswww/admin.php?m=design&c=push&a=shield||
+|admin|backup/backup/run|127.0.0.1|1487859258|/pwbbswww/admin.php?m=backup&c=backup||
+|admin|backup/backup/restore|127.0.0.1|1487859297|/pwbbswww/admin.php?m=backup&c=backup&a=restore||
+|admin|bbs/cache/run|127.0.0.1|1487859303|/pwbbswww/admin.php?m=bbs&c=cache||
+|admin|hook/manage/run|127.0.0.1|1487859306|/pwbbswww/admin.php?m=hook&c=manage||
+|admin|hook/manage/detail|127.0.0.1|1487859312|/pwbbswww/admin.php?name=c_cate_run&m=hook&c=manage&a=detail||
+|admin|hook/manage/run|127.0.0.1|1487859333|/pwbbswww/admin.php?m=hook&c=manage||
+|admin|cron/cron/run|127.0.0.1|1487859340|/pwbbswww/admin.php?m=cron&c=cron||
+|admin|cron/cron/add|127.0.0.1|1487859344|/pwbbswww/admin.php?m=cron&c=cron&a=add||
+|admin|cron/cron/run|127.0.0.1|1487859377|/pwbbswww/admin.php?m=cron&c=cron||
+|admin|log/manage/run|127.0.0.1|1487859378|/pwbbswww/admin.php?m=log&c=manage||
+|admin|log/loginlog/run|127.0.0.1|1487859394|/pwbbswww/admin.php?m=log&c=loginlog||
+|admin|nav/nav/run|127.0.0.1|1487859415|/pwbbswww/admin.php?m=nav&c=nav||
+|admin|config/regist/run|127.0.0.1|1487859417|/pwbbswww/admin.php?m=config&c=regist||
+|admin|Login Successful|127.0.0.1|1487864201|/pwbbswww/admin.php?a=login|username=admin, password=***, submit=, csrf_token=e630b185c676b5f3|
+|admin|default/founder/run|127.0.0.1|1487864248|/pwbbswww/admin.php?c=founder||
+|admin|default/founder/run|127.0.0.1|1487864250|/pwbbswww/admin.php?c=founder||
+|admin|default/auth/run|127.0.0.1|1487864322|/pwbbswww/admin.php?c=auth||
+|admin|default/safe/run|127.0.0.1|1487864325|/pwbbswww/admin.php?c=safe||
+|admin|windidclient/windid/run|127.0.0.1|1487864330|/pwbbswww/admin.php?m=windidclient&c=windid||
+|admin|default/founder/run|127.0.0.1|1487864340|/pwbbswww/admin.php?c=founder||
+|admin|Login Successful|127.0.0.1|1487869070|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=0a2b39454def5353|
+|admin|config/config/run|127.0.0.1|1487869117|/admin.php?m=config&c=config||
+|admin|config/config/dorun|127.0.0.1|1487869185|/admin.php?m=config&c=config&a=dorun|infoName=IT学院, infoUrl=http://www.pwbbs.com/, infoMail=admin@phpwind.com, infoIcp=, statisticscode=, visitState=0, visitIp=, visitMember=, visitMessage=站点升级中。。。, csrf_token=0a2b39454def5353|
+|admin|config/config/run|127.0.0.1|1487869188|/admin.php?m=config&c=config||
+|admin|config/config/run|127.0.0.1|1487869235|/admin.php?m=config&c=config||
+|admin|default/founder/run|127.0.0.1|1487869385|/admin.php?c=founder||
+|admin|default/auth/run|127.0.0.1|1487869392|/admin.php?c=auth||
+|admin|bbs/cache/run|127.0.0.1|1487869398|/admin.php?m=bbs&c=cache||
+|admin|bbs/cache/dorun|127.0.0.1|1487869403|/admin.php?m=bbs&c=cache&a=dorun||
+|admin|bbs/cache/doTpl|127.0.0.1|1487869408|/admin.php?m=bbs&c=cache&a=doTpl||
+|admin|Login Successful|127.0.0.1|1487948981|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=6fa725aa6941804f|
+|admin|Login Successful|127.0.0.1|1487954991|/admin.php?a=login|username=admin, password=***, submit=, csrf_token=6fa725aa6941804f|
+|admin|config/config/run|127.0.0.1|1487955154|/admin.php?m=config&c=config||
